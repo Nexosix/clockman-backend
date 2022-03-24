0dc373361f93749e36f414521566bf5c97fd457d
Paweł Drozgowski <pawel_drozgowski@zut.edu.pl>
2022-03-24 14:10:52 +0100

    - add register endpoint - add database - add prisma - add socket.io structure

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,21 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import { Server } from "socket.io";
+import http from 'http';
+import routes from './routes';
+
 
 const port = 5000;
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req: Request, res: Response) => res.send('Hello'));
+app.use(express.json());
+app.use('/', routes);
+app.get('/client', (req: Request, res: Response) => res.sendFile("public/index.html", { root: "." }));
 
-app.listen(port, () => {console.log(`Server listening on port ${port}`)});
+io.on('connection', (socket) => {
+    console.log('a user connected');
+  });
+
+server.listen(port, () => {console.log(`Server listening on port ${port}`)});
